737e72ca987d8dc400de388f511c6428e983d5b8
Nurudeen <nurudeensultan28@gmail.com>
2023-03-25 06:30:48 +0100

    Deletions

--- 0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-ing puppet to create a file
-file { '/tmp/school':
-    ensure  => 'file',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
-}
